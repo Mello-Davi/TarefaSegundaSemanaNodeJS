5e9a6fae98ddd56b97ee639868322f2d13e0fc74
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-02-24 15:30:19 +0100

    feat(/presenter/comment-presenter.ts): criação do presenter dos comentários

---
+++ src/libs/http/presenters/comment-presenter.ts
@@ -0,0 +1,23 @@
+import type { Comentario } from "@/@types/prisma/client"
+
+type HTTPComentario = {
+    id: string
+    conteudo: string
+    createdAt: Date 
+}
+
+export class ComentarioPresenter {
+    static toHTTP(comentario: Comentario): HTTPComentario
+    static toHTTP(comentarios: Comentario[]): HTTPComentario[]
+    static toHTTP(input: Comentario | Comentario[]): HTTPComentario | HTTPComentario[] {
+        if(Array.isArray(input)){
+            return input.map((Comentario)=> this.toHTTP(Comentario))
+        }
+
+        return{
+            id: input.publicId,
+            conteudo: input.conteudo,
+            createdAt: input.created_at
+        }
+    }
+}
