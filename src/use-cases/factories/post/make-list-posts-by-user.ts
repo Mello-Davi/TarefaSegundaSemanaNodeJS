53cb9394b8089c69f75997364bf1e53bc4583e23
Davi Rodrigues de Mello <davirmelo16@gmail.com>
2026-02-26 23:53:54 +0100

    refactor: remoção de importação não utilizada em make-list-posts-by-user.ts

--- src/use-cases/factories/post/make-list-posts-by-user.ts
+++ src/use-cases/factories/post/make-list-posts-by-user.ts
@@ -1,5 +1,4 @@
 import { PrismaPostsRepository } from "@/repositories/prisma/posts-prisma-repository"
-import { ListPostUseCase } from "@/use-cases/posts/list-posts"
 import { ListPostByUserUseCase } from "@/use-cases/posts/list-posts-by-user"
 
 
